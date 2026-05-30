15111b2cf7f0c73ece833549973870d911e6b92c
kaneki-212903 <132456512+kaneki-212903@users.noreply.github.com>
2026-05-30 17:13:56 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution:
+    def pair(self,s,t):
+        return Counter(s)==Counter(t)
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash={}
+        for s in strs:
+            key="".join(sorted(s))
+            if key not in hash:
+                hash[key]=[]
+            hash[key].append(s)
+        return list(hash.values())
